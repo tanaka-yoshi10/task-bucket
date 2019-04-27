d9d4041389bd0fe52715c1b3d6c8fa066eed3bd9
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-04-27 12:17:21 +0900

    basic認証かけられるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  protect_from_forgery with: :exception
+
+  private
+
+  def basic_auth
+    if ENV["BASIC_AUTH_USER"] && ENV["BASIC_AUTH_PASSWORD"]
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      end
+    end
+  end
 end
