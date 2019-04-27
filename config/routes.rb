b11b4c02480aefa20be5ffa3dcab413c03b5b894
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-04-27 12:20:42 +0900

    route設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
   resources :tasks do
     member do
       put :start
@@ -6,5 +7,4 @@ Rails.application.routes.draw do
     end
   end
   resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
