0cc8c437e3f5cab29b52bb55d38177df61934eaa
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-04-30 07:10:06 +0900

    変更の確定はボタンを押したときにする

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -2,9 +2,4 @@ import $ from 'jquery'
 import 'timepicker/jquery.timepicker'
 
 $(() => {
-  $(document).on('blur', '.js-task-row .js-task-input', (e) => {
-    const $target = $(e.target)
-    const $row = $target.closest('.js-task-row')
-    $row.find('input[type="submit"]').click()
-  })
 })
