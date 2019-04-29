c14317f48bc9c7f8c1830a8045bb46d81ca50ee9
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-04-30 06:34:16 +0900

    時刻だけを変えたいときに日付がかわらないように

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,7 @@ class Task < ApplicationRecord
   validates :scheduled_on, presence: true
 
   before_validation :set_scheduled_on
+  before_validation :set_time
 
   def actual
     return nil if start_at.blank? || end_at.blank?
@@ -18,10 +19,35 @@ class Task < ApplicationRecord
   private
 
   def set_scheduled_on
+    if scheduled_on.blank?
+      if start_at.present?
+        self.scheduled_on = start_at
+      else scheduled_on.blank?
+        self.scheduled_on = Time.current
+      end
+    end
+  end
+
+  def set_time
     if start_at.present?
-      self.scheduled_on = start_at
-    elsif scheduled_on.blank?
-      self.scheduled_on = Time.current
+      self.start_at = Time.zone.local(
+        scheduled_on.year,
+        scheduled_on.month,
+        scheduled_on.day,
+        start_at.hour,
+        start_at.min,
+        start_at.sec
+      )
+    end
+    if end_at.present?
+      self.end_at = Time.zone.local(
+        scheduled_on.year,
+        scheduled_on.month,
+        scheduled_on.day,
+        end_at.hour,
+        end_at.min,
+        end_at.sec
+      )
     end
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Task, type: :model do
   it 'has a valid factory' do
     expect(build(:task)).to be_valid
   end
+
+  describe '日付' do
+    it '日付が変わらないこと' do
+      task = create(:task, scheduled_on: '2019-04-28', start_at: '2019-04-28 7:00', end_at: '2019-04-28 8:00')
+      expect(task.scheduled_on).to eq Date.new(2019, 4, 28)
+      expect(task.start_at).to eq Time.zone.local(2019, 4, 28, 7, 0, 0)
+      expect(task.end_at).to eq Time.zone.local(2019, 4, 28, 8, 0, 0)
+
+      task.update(start_at: '8:00', end_at: '9:00')
+      expect(task.scheduled_on).to eq Date.new(2019, 4, 28)
+      expect(task.start_at).to eq Time.zone.local(2019, 4, 28, 8, 0, 0)
+      expect(task.end_at).to eq Time.zone.local(2019, 4, 28, 9, 0, 0)
+    end
+  end
 end
