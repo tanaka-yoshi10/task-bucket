d065c040cff30ae29b4fbab09c989053de0ef0fb
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-04-30 07:55:33 +0900

    編集モードに切り替わるように

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -1,5 +1,24 @@
 import $ from 'jquery'
 import 'timepicker/jquery.timepicker'
 
-$(() => {
+$(document).on('click', '.js-task-view', (event) => {
+  const target = $(event.target)
+  const row = target.closest('.js-task-row')
+  const view = row.find('.js-task-view')
+  const edit = row.find('.js-task-edit')
+
+  view.addClass('d-none')
+  edit.removeClass('d-none')
+})
+
+$(document).on('click', '.js-submit-task', (event) => {
+  const target = $(event.target)
+  const row = target.closest('.js-task-row')
+  const view = row.find('.js-task-view')
+  const edit = row.find('.js-task-edit')
+
+  view.removeClass('d-none')
+  edit.addClass('d-none')
+
+  // TODO: viewを更新したい(rowごとかな)
 })
