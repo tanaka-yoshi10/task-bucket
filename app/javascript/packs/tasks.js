f803472d47b4c309e9efcb6b501552e8cf2c116b
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-04-30 16:13:01 +0900

    iOSでclickが効かない対策

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -1,14 +1,16 @@
 import $ from 'jquery'
 import 'timepicker/jquery.timepicker'
 
-$(document).on('click', '.js-task-view', (event) => {
-  const target = $(event.target)
-  const row = target.closest('.js-task-row')
-  const view = row.find('.js-task-view')
-  const edit = row.find('.js-task-edit')
+$(() => {
+  $('.container-fluid').on('click', '.js-task-view', (event) => {
+    const target = $(event.target)
+    const row = target.closest('.js-task-row')
+    const view = row.find('.js-task-view')
+    const edit = row.find('.js-task-edit')
 
-  view.addClass('d-none')
-  edit.removeClass('d-none')
+    view.addClass('d-none')
+    edit.removeClass('d-none')
+  })
 })
 
 $(document).on('click', '.js-submit-task', (event) => {
