e8ca53ac171bba9f7be19541799af8a67bf0841e
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-05-11 08:09:42 +0900

    プロジェクトの色の初期値

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ApplicationRecord
   has_many :routines, dependent: :nullify
   belongs_to :user
 
+  attribute :color, :string, default: '#6c757d'
+
   validates :name, presence: true
 
   scope :default_order, -> { order(:name, :id) }
