f8eeda95a948fe8db3ed8897aa1cede6e4e5a24d
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-05-26 07:33:08 +0900

    rubocop指摘

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,13 +1,14 @@
 module TasksHelper
   def tasks_to_calendar_json(tasks)
-    tasks.map do |task|
+    events = tasks.map do |task|
       {
         title: task.title,
         start: task.start_at.iso8601,
         end: task.end_at.iso8601,
         backgroundColor: task.project&.color,
-        textColor: 'white'
+        textColor: 'white',
       }
-    end.to_json
+    end
+    events.to_json
   end
 end
