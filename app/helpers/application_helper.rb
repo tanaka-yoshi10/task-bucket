b2714dfbddfcd1edc8333a6e9b337b7f9d1018c2
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-06-08 13:03:17 +0900

    refactor: nlを導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     str = html_escape(str)
     raw str.gsub(/\r\n|\r|\n/, '<br />') # rubocop:disable Rails/OutputSafety
   end
+
+  def null_check_localization(*args)
+    localize(*args) if args&.first.present?
+  end
+  alias nl null_check_localization
 end
