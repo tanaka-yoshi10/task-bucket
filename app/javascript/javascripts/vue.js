93aa50947541240b610056fc62e003743f6c78db
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-07-06 15:18:35 +0900

    vueに置き換えてみる

--- app/javascript/javascripts/vue.js
+++ app/javascript/javascripts/vue.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import vueCustomElement from 'vue-custom-element'
+import TheTaskList from '../components/TheTaskList.vue'
 
 Vue.use(vueCustomElement)
+
+Vue.customElement('the-task-list', TheTaskList)
 
--- app/javascript/packs/dashboard_vue.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue.esm.js'
-import Index from './components/index.vue'
-
-var app = new Vue({
-  el: '#app',
-  components: {
-    'container': Index,
-  }
-});
