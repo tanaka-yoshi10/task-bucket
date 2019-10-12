a039d58c0b90f1e77e2921a8dcb9f2221f33ffb3
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-10-12 20:27:47 +0900

    開始と終了表示

--- app/javascript/components/Task.vue
+++ app/javascript/components/Task.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
     <div @click="toggle">
-      <div class="my-2">
-        {{ task.title }}
-      </div>
-      <div class="form-inline" />
-      <div class="my-2" />
       <div v-if="editable">
-        初期表示の内容
+        編集画面
       </div>
       <div v-else>
-        クリックで切り替わった内容
+        <div class="my-2">
+          {{ task.title }}
+        </div>
+        <div class="form-inline">
+          {{ task.start_at | timeFormat }}
+          〜
+          {{ task.end_at | timeFormat }}
+          実績:
+          14
+        </div>
+        <div class="my-2"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { format as formatDate, parseISO } from 'date-fns'
+
 export default {
   props: {
     task: {
@@ -32,12 +39,18 @@ export default {
   computed: {
   },
   mounted() {
+    console.log(formatDate(parseISO(this.task.start_at), 'hh:mm'))
   },
   methods: {
     toggle() {
       this.editable = !this.editable
     },
   },
+  filters: {
+    timeFormat(time) {
+      return formatDate(parseISO(time), 'hh:mm')
+    }
+  }
 }
 </script>
 
