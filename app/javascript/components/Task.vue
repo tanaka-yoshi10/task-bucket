2c8f2d4bd856e9e8e2a91b2bb5916c74ee1db3b9
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-10-14 08:38:52 +0900

    見た目を修正

--- app/javascript/components/Task.vue
+++ app/javascript/components/Task.vue
@@ -126,60 +126,24 @@
           >
             <i class="fas fa-stop"></i>
           </button>
-          <a
-            class="mx-2 btn btn-danger"
-            data-remote="true"
-            rel="nofollow"
-            data-method="put"
-            href="/tasks/157/complete"
-          >
-            <i class="fas fa-stop"></i>
-          </a>
-          <a
+          <button
             class="mx-2 btn"
-            data-remote="true"
-            rel="nofollow"
-            data-method="put"
-            href="/tasks/157/pause"
-          ><svg
-            class="svg-inline--fa fa-pause fa-w-14"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="pause"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            data-fa-i2svg=""
-          ><path fill="currentColor" d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z" /></svg><!-- <i class="fas fa-pause"></i> --></a>
-          <a class="btn js-edit-task" href="javascript:void(0)"><svg
-            class="svg-inline--fa fa-cog fa-w-16"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="cog"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            data-fa-i2svg=""
-          ><path fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z" /></svg><!-- <i class="fas fa-cog"></i> --></a>
-          <a
-            class="btn"
-            data-remote="true"
-            rel="nofollow"
-            data-method="put"
-            href="/tasks/157/clone"
-          ><svg
-            class="svg-inline--fa fa-clone fa-w-16"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="clone"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            data-fa-i2svg=""
-          ><path fill="currentColor" d="M464 0c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48H176c-26.51 0-48-21.49-48-48V48c0-26.51 21.49-48 48-48h288M176 416c-44.112 0-80-35.888-80-80V128H48c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48h288c26.51 0 48-21.49 48-48v-48H176z" /></svg><!-- <i class="fas fa-clone"></i> --></a>
+            @click="pause(task)"
+          >
+            <i class="fas fa-pause"></i>
+          </button>
+          <button
+            class="mx-2 btn"
+            @click="toggle"
+          >
+            <i class="fas fa-cog"></i>
+          </button>
+          <button
+            class="mx-2 btn"
+            @click="clone"
+          >
+            <i class="fas fa-clone"></i>
+          </button>
         </div>
       </div>
     </div>
@@ -228,7 +192,13 @@ export default {
         console.log(response)
       }, (error) => {
       })
-    }
+    },
+    pause() {
+      // TODO: 中断処理
+    },
+    clone() {
+      // TODO: 複製処理
+    },
   },
 }
 </script>
